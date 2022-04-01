013c358d25bcdf54fca4870a06599f944d5eeb87
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-04-01 14:51:34 -0500

    First Commit. 4-1-22. Created ID Functions with HTML & JavaScript.

---
+++ app.js
@@ -0,0 +1,6 @@
+function run() {
+    document.getElementById("paragraph").innerHTML = "Hey look you did it!";
+    document.getElementById("paragraph").style.backgroundColor = "green";
+    document.getElementById("paragraph").style.color = "white";
+    document.getElementById("paragraph").style.padding = "15px";
+}
