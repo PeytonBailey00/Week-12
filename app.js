2234e2ee7392123731a490e9d6228c7fccecced3
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-04-04 14:50:30 -0500

    Second Commit. 4-4-22. Created Random Number Function & Compounded Functions.

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 function run() {
-    document.getElementById("paragraph").innerHTML = "Hey look you did it!";
+    document.getElementById("paragraph").innerHTML = "Hello World!";
     document.getElementById("paragraph").style.backgroundColor = "green";
     document.getElementById("paragraph").style.color = "white";
     document.getElementById("paragraph").style.padding = "15px";
+    document.getElementById("paragraph").style.textAlign = "center";
+    randomNum();
+}
+
+function randomNum() {
+    let ran = Math.floor(Math.random() * 10)
+    let a = document.getElementById("random")
+    a.innerHTML = ran;
+    a.style.backgroundColor = "blue";
+    a.style.color = "white";
+    a.style.padding = "15px";
+    a.style.textAlign = "center";
 }
