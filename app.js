64f47f44f3d549502a8dda459167c57422b60934
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-04-06 14:51:24 -0500

    Fourth Commit. 4-6-22. Finalized Code & added Cosmetic Improvements.

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ userNum.focus();
 let counter = 0;
 
 function run() {
-    let a = document.getElementById("paragraph")
-    a.innerHTML = "Hello World!";
-    a.style.backgroundColor = "purple";
-    a.style.color = "white";
-    a.style.padding = "15px";
-    a.style.textAlign = "center";
     randomNum();
     userNumber();
     compareNumbers();
@@ -23,23 +17,11 @@ function run() {
 
 function randomNum() {
     let ran = Math.floor(Math.random() * 10)
-    let b = document.getElementById("random")
-    b.innerHTML = ran;
-    b.style.backgroundColor = "blue";
-    b.style.color = "white";
-    b.style.padding = "15px";
-    b.style.textAlign = "center";
     return ran;
 }
 
 function userNumber() {
     let user = document.getElementById("getNumber").value;
-    let c = document.getElementById("userNumber")
-    c.innerHTML = user;
-    c.style.backgroundColor = "orange";
-    c.style.color = "white";
-    c.style.padding = "15px";
-    c.style.textAlign = "center";
     return user;
 }
 
@@ -51,28 +33,30 @@ function compareNumbers() {
 
     if (x != y) {
         d.innerHTML = "Numbers are not equal. Computer got " + y + ", & user got " + x;
-        d.style.backgroundColor = "red";
+        d.style.backgroundColor = "#782222";
         d.style.color = "white";
         d.style.padding = "15px";
         d.style.textAlign = "center";
         counter++
         e.innerHTML = "You have made " + counter + " attempts."
-        e.style.backgroundColor = "gray";
+        e.style.backgroundColor = "#191919";
         e.style.color = "white";
         e.style.padding = "15px";
         e.style.textAlign = "center";
+        document.querySelector("body").style.backgroundColor ="#191919"
     } else if (x == y) {
         d.innerHTML = "Numbers are equal. Computer got " + y + ", & user got " + x;
-        d.style.backgroundColor = "green";
+        d.style.backgroundColor = "#255417";
         d.style.color = "white";
         d.style.padding = "15px";
         d.style.textAlign = "center";
         counter++
         e.innerHTML = "You took " + counter + " attempts to get it right."
-        e.style.backgroundColor = "gray";
+        e.style.backgroundColor = "#191919";
         e.style.color = "white";
         e.style.padding = "15px";
         e.style.textAlign = "center";
+        document.querySelector("body").style.backgroundColor ="#191919"
     }
     resetInput();
 }
